4276bd4bd294d6b1c25eb55a96cf5e50b1352d15
Dandy Do <47428418+DandyDo@users.noreply.github.com>
2020-09-06 19:36:16 -0400

    Camera can now follow and rotate around the player
    + moved everything to LateUpdate (CameraHandler script)

--- Assets/Scripts/CameraHandler.cs
+++ Assets/Scripts/CameraHandler.cs
@@ -10,10 +10,16 @@ public class CameraHandler : MonoBehaviour
     public float zoomOutMax = 80;
 
 
-
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
+        // Position the main camera to point at the player's position
+        transform.position = player.position + camOffset;
+    }
+
+    private void LateUpdate()
+    {
+        //Follow the player's position
+        transform.position = player.position + camOffset;
 
         // Rotate the camera around the player
         if (Input.touchCount == 1)
@@ -25,7 +31,7 @@ public class CameraHandler : MonoBehaviour
             float touchZeroCurr = touchZero.position.x - touchZeroPrevPos.x;
 
             transform.RotateAround(player.position, Vector3.up, touchZeroCurr * 0.06f);
-
+            camOffset = transform.position - player.position;
         }
 
         // Pinch-zoom the camera's field of view
@@ -38,7 +44,7 @@ public class CameraHandler : MonoBehaviour
             Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
 
             float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
-            float currentMagnitude = (touchZero.position- touchOne.position).magnitude;
+            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
 
             float difference = currentMagnitude - prevMagnitude;
 
@@ -46,14 +52,9 @@ public class CameraHandler : MonoBehaviour
         }
     }
 
-    private void LateUpdate()
-    {
-        //Follow the player's position
-        //transform.position = player.position + camOffset;
-    }
-
     void zoom(float increment)
     {
         Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView - increment, zoomOutMin, zoomOutMax);
+        
     }
 }
