b3d2d321cb9a174f7e47f17a3b439e0be7ba35da
Dandy Do <47428418+DandyDo@users.noreply.github.com>
2020-09-05 22:03:26 -0400

    Added features (also find a solution to camera follow + rotation)
    Added camera handler object:
    - Rotation around the play with one touch swipe
    - Zoom capability with pinch touch
    - camera follows player (does not work well with rotatio - NEEDS FIX)
    
    Added a debug script for collision detection (enter and stay) - This should be an easy way to detect if player is in the building or not.
    - Issues: must detect the nerest buildings to the player's location, which then names them and sets their location to the actual game Scene.
    ^ using that method will cause the players to use a GPS map (current scene name: Map) to navigate between Hubs.
    
    NOTE: Changed between the GPS provider script (editor and device locator) for debugging purposes.

---
+++ Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHandler : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 camOffset = new Vector3(0, 60, -60);
+    public float zoomOutMin = 20;
+    public float zoomOutMax = 80;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Rotate the camera around the player
+        if (Input.touchCount == 1)
+        {
+            Touch touchZero = Input.GetTouch(0);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+
+            float touchZeroCurr = touchZero.position.x - touchZeroPrevPos.x;
+
+            transform.RotateAround(player.position, Vector3.up, touchZeroCurr * 0.06f);
+
+        }
+
+        // Pinch-zoom the camera's field of view
+        if (Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float currentMagnitude = (touchZero.position- touchOne.position).magnitude;
+
+            float difference = currentMagnitude - prevMagnitude;
+
+            zoom(difference * 0.05f);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        //Follow the player's position
+        //transform.position = player.position + camOffset;
+    }
+
+    void zoom(float increment)
+    {
+        Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView - increment, zoomOutMin, zoomOutMax);
+    }
+}
---
+++ Assets/Scripts/CollisionDetect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetect : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("I have collided with the building");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("I have stayed with the building");
+    }
+}
