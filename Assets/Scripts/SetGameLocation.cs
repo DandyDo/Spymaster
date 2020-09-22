fd64f28dace5a5032f14b3ec66e21b7d8dda50ad
Dandy Do <47428418+DandyDo@users.noreply.github.com>
2020-09-22 05:52:29 -0400

    SetGameLocation completed (still need to find a way to run it without IEnumerator)
    - script now finds buildings near player, if list did not fill up (10 buildings), it will fetched the next building closest to each building in the list.
    - script changes the names of the hubs (from 0 to 9).
    - script changes the color of the buildings (array of colors of length 10 (0->9) set in the editor).
    - script sets a marker prefab, matching the color of the building.
    
    * Created a new prefab. PoiMarkerHolder. This is to mark the buildings in the list.
    
    ** You have to set the building spawn position to center of vertices. This is to allow us to position the objects relative to the buildings in the list.

--- Assets/Scripts/SetGameLocation.cs
+++ Assets/Scripts/SetGameLocation.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 using Mapbox.Map;
 using Mapbox.Unity.Map;
 using System.Linq;
+using Mapbox.Platform.Cache;
+using Mapbox.Examples;
+using UnityEngine.UI;
 
 public class SetGameLocation : MonoBehaviour
 {
@@ -13,7 +16,9 @@ public class SetGameLocation : MonoBehaviour
     List<Collider> colliders = new List<Collider>();
     public Transform player;
     public float playerRadius = 30;
-    public float buildingRadius = 20; // Do not go below 9. Causes Unity to freeze for no reason. (find fix when you have time)
+    public float buildingRadius = 20;
+    public GameObject prefab;
+    public Color[] colors;
 
     // Bit shift the index of the layer 8 (player) to get a bit mask
     int layerMask = 1 << 8;
@@ -21,7 +26,7 @@ public class SetGameLocation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // invert bit mask to ignore layer 8 specifically.
+        // Invert bit mask to ignore layer 8 (player) specifically.
         layerMask = ~layerMask;
 
         StartCoroutine(GetClosestBuildings());
@@ -30,15 +35,24 @@ public class SetGameLocation : MonoBehaviour
     // Wait for n seconds before addings buildings within radius of player to a list
     IEnumerator GetClosestBuildings()
     {
-        yield return new WaitForSeconds(2);
-        Debug.Log("waited 3 seconds");
+        yield return new WaitForSeconds(5);
+        Debug.Log("waited 5 seconds");
 
-        // search for the closest buildings to the player
+        // Search for the closest buildings to the player
         colliders = Physics.OverlapSphere(player.transform.position, playerRadius, layerMask).ToList();
+
+        colliders.Sort(DistanceToPlayer);
 
         foreach (Collider collider in colliders)
         {
-            buildings.Add(collider.gameObject);
+            if (buildings.Count < 10)
+            {
+                buildings.Add(collider.gameObject);
+            }
+            else
+            {
+                break;
+            }
         }
 
         Debug.Log("There are " + buildings.Count + " buildings in the list. GetClosestBuildings");
@@ -48,43 +62,98 @@ public class SetGameLocation : MonoBehaviour
         yield return FetchBuildings();
     }
 
-    // set the list with all the buildings after GetClosestBuildings()
+    // Fetch the rest of the buildings if the list wasn't filled after GetClosestBuildings()
     IEnumerator FetchBuildings()
     {
-        while (buildings.Count < 10)
+        int i = buildings.Count;
+        while (buildings.Count < 10 && i < 10)
         {
             List<GameObject> tempBuildings = new List<GameObject>();
 
             foreach (GameObject building in buildings)
             {
-                if (Physics.CheckSphere(building.transform.position, buildingRadius, layerMask)) // Check if there are nearby building close to the curr building
+                // Check if there are nearby building close to the curr building
+                if (Physics.CheckSphere(building.transform.position, buildingRadius, layerMask))
                 {
                     colliders = Physics.OverlapSphere(building.transform.position, buildingRadius, layerMask).ToList();
+
                     foreach (Collider collider in colliders)
                     {
                         if (!buildings.Contains(collider.gameObject))
                         {
-                            if (tempBuildings.Count < 10 - buildings.Count) // check if buildings found so far are less than 10
+                            // check if buildings found so far are less than 10
+                            if (tempBuildings.Count < 10 - buildings.Count) 
                             {
                                 tempBuildings.Add(collider.gameObject);
                             }
-                            else
-                            {
-                                break;
-                            }
-
                         }
+
                         Debug.Log("There are " + tempBuildings.Count + " buildings in the temp list.");
                     }
 
                     colliders.Clear();
-                }
+                }               
             }
+
+            i++;
 
             buildings.AddRange(tempBuildings);
             Debug.Log("There are " + buildings.Count + " buildings in the list.");
         }
 
+        yield return EditBuildings();
+    }
+
+    // Modify the selected buildings. (Sort buildings by distance from player, buildings' names, and colors)
+    IEnumerator EditBuildings()
+    {
+        // Sort the returned compared result from the function
+        buildings.Sort(DistanceToPlayer);
+
+        // Name each buildings from 0 to 9
+        for (int j = 0; j < buildings.Count; j++)
+        {
+            buildings[j].name = j.ToString();
+        }
+
+        // Instantiate a prefarb (Poi Marker) on each buildings in the list
+        for (int i = 0; i < buildings.Count; i++)
+        {
+            var newPrefab = Instantiate(prefab, buildings[i].transform.position, Quaternion.identity, buildings[i].transform);
+
+            newPrefab.GetComponentInChildren<Text>().text = buildings[i].name;
+
+            var buildingRenderer = buildings[i].GetComponent<Renderer>().materials;
+            var newPrefabRenderer = newPrefab.GetComponentInChildren<SpriteRenderer>();
+
+            foreach (Material material in buildingRenderer)
+            {
+                material.SetColor("_Color", colors[i]);
+            }
+            //buildingRenderer.material.SetColor("_Color", colors[i]);      // for top-side (roof) color only
+            newPrefabRenderer.material.SetColor("_Color", colors[i]);
+        }
+
         yield break;
     }
+
+    int DistanceToPlayer (GameObject a, GameObject b)
+    {
+        // Find each sqrMagnitude of each object from player and compare
+        float x = (a.transform.position - player.position).sqrMagnitude;
+        float y = (b.transform.position - player.position).sqrMagnitude;
+
+        return x.CompareTo(y);
+    }
+
+    int DistanceToPlayer (Collider a, Collider b)
+    {
+        // Find each sqrMagnitude of each object from player and compare
+        float x = (a.transform.position - player.position).sqrMagnitude;
+        float y = (b.transform.position - player.position).sqrMagnitude;
+
+        return x.CompareTo(y);
+    }
 }
+
+
