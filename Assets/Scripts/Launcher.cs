42b784b0a4440170b05cea1d05b1cc7d9ad66561
Dandy Do <47428418+DandyDo@users.noreply.github.com>
2020-12-12 20:40:31 -0500

    Cleaning up the UI + Starting up rooms in Photon
    - Added a UI Manager script for menus. Now all menu buttons can  use the script for easy open/close menus.
    - Players can now host a room through Photon (still not joinable nor foundable [not implemented]).

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    [SerializeField] TMP_InputField roomNameInputField;
+    [SerializeField] TMP_Text errorText;
+    [SerializeField] TMP_Text roomNameText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Connecting to Master...");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        //Debug.Log("Connected to Master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        MenuManager.Instance.OpenMenu("main menu");
+        //Debug.Log("Joined Lobby");
+    }
+
+    public void CreateRoom()
+    {
+        if (string.IsNullOrEmpty(roomNameInputField.text))
+        {
+            return;
+        }
+
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.Instance.OpenMenu("main menu");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        MenuManager.Instance.OpenMenu("room");
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        errorText.text = "Failed to create room. Error: " + message;
+        MenuManager.Instance.OpenMenu("error");
+    }
+}
