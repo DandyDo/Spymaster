3bb7d50e6aa3c76af83dce18aef41954b3564d10
Noah Moyer <35582108+NoahMoyer@users.noreply.github.com>
2020-12-20 14:12:32 -0500

    Working on adding cards to the game

--- Assets/Scripts/Cards.cs
+++ Assets/Scripts/Cards.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class Cards : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string cardName;
+    public bool used = false;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
---
+++ Assets/Scripts/CardsManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CardsManager : MonoBehaviour
+{
+    public List<Cards> cardList = new List<Cards>();
+    Cards cardToBeUsed;
+    public TMP_Text confirmationText;
+    public void setCardToBeUsed(Cards cardToBeUsedInput)
+    {
+        cardToBeUsed = cardToBeUsedInput;
+        confirmationText.text = "Use " + cardToBeUsed.cardName + "?";
+    }
+    public void useCard()
+    {
+        foreach (Cards card in cardList)
+        {
+            if (cardToBeUsed.name == card.name)
+            {
+                //use card
+                Debug.Log("Used " + cardToBeUsed.cardName);
+                card.used = true;
+            }
+        }
+    }
+}
